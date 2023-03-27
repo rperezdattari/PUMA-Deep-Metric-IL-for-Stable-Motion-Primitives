Quaternion flip fixed using previous value

src/data_preprocessing/data_loader.py
@@ -1,5 +1,6 @@
 from datasets.dataset_keys import LASA, LASA_S2, LAIR, optitrack, interpolation, joint_space, lieflows, ABB, ABB_R3S3
 from scipy.spatial.transform import Rotation
+from spatialmath import SO3, UnitQuaternion
 import os
 import pickle
 import numpy as np
@@ -207,12 +208,31 @@ def load_ABB_S3R3(dataset_dir, demonstrations_names):
             filename = dataset_dir + demonstrations_names[i] + '/' + demo_primitive
             with open(filename, 'rb') as file:
                 data = pickle.load(file)
-            rot = Rotation.from_matrix(np.array(data['x_rot']))
-            eul = rot.as_euler('xyz') * 30
+            #rot = Rotation.from_matrix(np.array(data['x_rot']))
+            #eul = rot.as_euler('xyz') * 30
             #plot_points_3d(eul)
-            rot = Rotation.from_euler('xyz', eul)
-            quat = rot.as_quat()
-            demo = np.concatenate([np.array(data['x_pos']), quat], axis=1).T
+            #rot = Rotation.from_euler('xyz', eul)
+            #quat = rot.as_quat()
+            prev_quat = None
+            quats = []
+            for numpy_rot_mat in data['x_rot']:
+                # Get quatenion array from data
+                quat = UnitQuaternion(SO3(numpy_rot_mat)).A
+
+                # Check if quaternion flip
+                if prev_quat is None:
+                    prev_quat = quat
+
+                dist_quats = np.linalg.norm(quat - prev_quat)
+
+                if dist_quats > 0.5:
+                    quat *= -1
+
+                quats.append(quat)
+                prev_quat = quat
+
+            #quat = [UnitQuaternion(SO3(numpy_rot_mat)).A for numpy_rot_mat in data['x_rot']]
+            demo = np.concatenate([np.array(data['x_pos']), np.array(quats)], axis=1).T
             demos.append(demo)
             dt.append(data['delta_t'])
             primitive_id.append(i)

src/params/1st_order_R3S3.py
@@ -8,7 +8,7 @@ class Params:
     dataset_name: str = 'ABB_R3S3'  # selects dataset, options: LASA, LAIR, optitrack, interpolation, joint_space
     results_path: str = 'results/1st_order_R3S3/'
     multi_motion: bool = False  # true when learning multiple motions together
-    selected_primitives_ids: str = '0'  # id number from dataset_keys.py, e.g., '2' or '4,0,6'
+    selected_primitives_ids: str = '1'  # id number from dataset_keys.py, e.g., '2' or '4,0,6'
     manifold_dimensions: int = 6  # dimensionality of the data manifold
     saturate_out_of_boundaries_transitions: bool = True  # True to enforce positively invariant set
     dynamical_system_order: int = 1  # options: 1, 2
@@ -31,14 +31,14 @@ class Params:
 
     """ Training """
     train: bool = True  # true when training
-    load_model: bool = True  # true to load previously trained model
+    load_model: bool = False  # true to load previously trained model
     max_iterations: int = 41000  # maximum number of training iterations
 
     """ Preprocessing """
     workspace_boundaries_type: str = 'custom'  # options: from data, custom
     workspace_boundaries: np.ndarray = np.array([[-1.0, 1.0],
                                                  [-1.0, 1.0],
-                                                 [-1.0, 1.0],
+                                                 [0.0, 2.0],
                                                  [-1.0, 1.0],
                                                  [-1.0, 1.0],
                                                  [-1.0, 1.0],
@@ -56,7 +56,7 @@ class Params:
     ignore_n_spurious: bool = False  # when selecting best model, true to ignore amount of spurious attractors
     fixed_point_iteration_thr = 0.2  # distance threshold to consider that a point did not reach the goal
     density: int = 2  # density^workspace_dimension = amount of points sampled from state space for evaluation
-    simulated_trajectory_length: int = 3100  # integration length for evaluation
+    simulated_trajectory_length: int = 1000  # integration length for evaluation
     evaluation_samples_length: int = 100  # integration steps skipped in quantitative evaluation for faster evaluation
     show_plotly: bool = True  # show evaluation during training
 