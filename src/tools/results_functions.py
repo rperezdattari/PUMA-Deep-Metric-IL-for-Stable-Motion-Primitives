1b28d348830d7ea87b9e9623bb6966140af24db9
Rodrigo <rodrigoperezd@gmail.com>
2023-04-08 15:34:37 +0200

    Commit before modifying code to add second order motions

--- src/tools/results_functions.py
+++ src/tools/results_functions.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 from initializer import initialize_framework
 from data_preprocessing.data_loader import load_demonstrations
 from datasets.dataset_keys import dataset_keys_dic
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.io as pio
+import plotly.graph_objects as go
+import pickle
 
 
 def get_number_of_primitives(dataset_name):
@@ -60,3 +67,39 @@ def evaluate_system_comparison(quanti_eval, quali_eval, models_names, dataset_na
             saturate = True
         evaluate_system(quanti_eval, quali_eval, model_name, dataset_name, demo_id, density,
                         simulated_trajectory_length, results_base_directory, saturate=saturate)
+
+
+def get_camera_parameters(dataset_name, model_name, demo_id, model):
+    plot_data = pickle.load(
+        open('results/final/%s/%s/%i/images/primitive_0_iter_%i.pickle' % (dataset_name, model_name, demo_id, model),
+             'rb'))
+
+    fig = go.Figure(data=plot_data['3D_plot'])
+
+    app = dash.Dash()
+    app.layout = html.Div([
+        html.Div(id="output"),  # use to print current relayout values
+        dcc.Graph(id="fig", figure=fig)
+    ])
+
+    @app.callback(Output("output", "children"), Input("fig", "relayoutData"))
+    def show_data(data):
+        # show camera settings like eye upon change
+        return [str(data)]
+
+    app.run_server(debug=True, use_reloader=False)
+
+
+def plot_LASA_S2(dataset_name, model_name, demo_id, model, camera):
+    save_path = 'results_analysis/%s_%s_%i.pdf' % (dataset_name, model_name, demo_id)
+    plot_data = pickle.load(
+        open('results/final/%s/%s/%i/images/primitive_0_iter_%i.pickle' % (dataset_name, model_name, demo_id, model),
+             'rb'))
+    fig = go.Figure(data=plot_data['3D_plot'])
+    fig.update_layout(scene=dict(camera=camera))
+
+    # Save image
+    pio.write_image(fig, save_path + '.pdf', width=500, height=500)
+
+    # Show
+    fig.show()
