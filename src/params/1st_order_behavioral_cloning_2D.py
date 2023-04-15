b526416541356881351939ff51dce3da2ec16081
Rodrigo <rodrigoperezd@gmail.com>
2023-04-15 18:47:41 +0200

    hyperparams optimized

--- src/params/1st_order_behavioral_cloning_2D.py
+++ src/params/1st_order_behavioral_cloning_2D.py
@@ -17,7 +17,7 @@ class Params:
     latent_space_dim: int = 300  # dimensionality latent space
     neurons_hidden_layers: int = 300  # number of neurons per layer
     batch_size: int = 250  # sampling batch size
-    learning_rate: float = 0.0001  # 0.0001  # AdamW learning rate
+    learning_rate: float = 0.0001964  # 0.0001  # 0.0001  # AdamW learning rate
     weight_decay: float = 0.0  # AdamW weight decay
 
     """ Contrastive Imitation """
@@ -25,7 +25,7 @@ class Params:
     imitation_loss_weight: float = 1  # imitation loss weight
     stabilization_loss_weight: float = 0  # stability loss weight
     boundary_loss_weight: float = 0  # boundary loss weight
-    imitation_window_size: int = 15  # 15  # imitation window size
+    imitation_window_size: int = 15  #15  # 15  # imitation window size
     stabilization_window_size: int = 14  # 6  # 2  # stability window size
     triplet_margin: float = 3.0122e-05  # 4.523e-9  # 1.25e-4  # triplet loss margin
     interpolation_sigma: float = 0.8  # percentage of points sampled in demonstrations space when multi-model learning
